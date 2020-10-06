a92aa8764ab6e7ca966d012c244439cbd9870525
lsvejdag8mb <svejda@g8mb.cz>
2020-10-06 13:41:52 +0000

    Výběr operace a kontrola vstupů.

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ function pozdravZMojiStranky() {
 function vypocet() {
   let c1 = document.getElementById("cislo1").valueAsNumber;
   let c2 = document.getElementById("cislo2").valueAsNumber;
-  let vysl = c1 + c2;
+  let op = document.getElementById("operace").value;
+  console.log(c1);
+
+  let vysl = "?";
+  if (isNaN(c1)) {
+    alert("Neni zadano prvni cislo!");
+  } else if (isNaN(c2)) {
+    alert("Neni zadano druhe cislo!");
+  } else if (op == "plus") {
+    vysl = c1 + c2;
+  } else if (op == "minus") {
+    vysl = c1 - c2;
+  }
+
   console.log(vysl);
+  document.getElementById("vysledek").value = vysl;
+  document.getElementById("vysledek2").innerHTML = "Výsledek je <b>" + vysl + "</b>.";
+
 }
